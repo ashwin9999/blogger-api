94a6fd92c60d4ff3aa90518402832db5e7b7faa4
Ashwin Mishra <amishra01@student.ysu.edu>
2020-10-06 18:26:27 -0400

    add user route to central router

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import * as Router from '@koa/router';
 
 import { KoaApp, KoaRouter } from './interface';
 
+import { router as userRouter } from './entities/user/user.routes';
 import { router as articleRouter } from './entities/article/article.routes';
 
 export const router: KoaRouter = (() => {
     const rtr: KoaRouter = new Router();
 
+    rtr.use('/user', userRouter.routes(), userRouter.allowedMethods());
     rtr.use('/article', articleRouter.routes(), articleRouter.allowedMethods());
 
     return rtr;
