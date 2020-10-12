85fee00ba9f92ed8e0157491a91c1bee6e04814b
Ashwin Mishra <amishra01@student.ysu.edu>
2020-10-12 19:55:11 -0400

    add unit test for getting article by id controller

--- src/entities/article/article.controller.spec.ts
+++ src/entities/article/article.controller.spec.ts
@@ -9,13 +9,14 @@ import * as sinon from 'sinon';
 import { StandardResponseFormat } from '../../interface';
 import { Context } from '../../interface';
 
-import { updateArticleController } from './article.controller';
+import { getArticleByIdController, updateArticleController } from './article.controller';
 import { Article } from './article.interface';
 
 import * as articleService from './article.service';
 
 @suite export class ArticleControllerTests {
     private updateArticle: sinon.SinonStub;
+    private getArticleById: sinon.SinonStub;
 
     private readonly article: Article = {
         id: 1,
@@ -33,10 +34,12 @@ import * as articleService from './article.service';
 
     before() {
         this.updateArticle = sinon.stub(articleService, 'updateArticle').resolves(this.article);
+        this.getArticleById = sinon.stub(articleService, 'getArticleById').resolves(this.article);
     }
 
     after() {
         this.updateArticle.restore();
+        this.getArticleById.restore();
     }
 
     @test async 'should update an article'() {
@@ -52,6 +55,24 @@ import * as articleService from './article.service';
 
         expect(this.updateArticle.called).to.be.true;
         expect(this.updateArticle.calledWithExactly(id, this.article));
+
+        expect(respondOk.called).to.be.true;
+        expect(respondOk.calledWithExactly(expected));
+    }
+
+    @test async 'should get an article by id'() {
+        const respondOk: sinon.SinonStub = sinon.stub();
+
+        const id = this.article.id;
+
+        const expected: StandardResponseFormat<Article> = { result: this.article, error: false };
+
+        const ctx: Context = <Context> <unknown> { respondOk };
+
+        await getArticleByIdController(ctx);
+
+        expect(this.getArticleById.called).to.be.true;
+        expect(this.getArticleById.calledWithExactly(this.article.id));
 
         expect(respondOk.called).to.be.true;
         expect(respondOk.calledWithExactly(expected));
